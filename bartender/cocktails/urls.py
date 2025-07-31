eb8fa188bb026dac70f8a41606c3fac5598b964e
Tushnamaitty <tushnamaittynakra@gmail.com>
2025-07-31 20:28:02 +0530

    Completed task 3

--- bartender/cocktails/urls.py
+++ bartender/cocktails/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Cocktail_List, name='cocktail_list'),
-    path('cocktail/<str:drink_id>/', views.cocktail_detail, name='cocktail_detail'),
+    path('cocktail/cocktail/<str:drink_id>/', views.cocktail_detail, name='cocktail_detail'),
 ]
